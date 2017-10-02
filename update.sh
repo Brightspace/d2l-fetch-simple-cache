b96648ee19eae5fcb760b6e98296d0690210081c
Ryan Mahler <Ryan.Mahler@D2L.com>
2017-10-02 17:46:04 -0400

    Make bower-able
    Adding this file allows us to use bower to control the versioning of this package, rather than using the CDNified versions and hoping that they won't conflict. This file is automatically updated by Travis to point at the CDN-ified script that will be generated by frau-ci in the next step. Since the new version isn't released until _after_ this commit is pushed, it does theoretically mean that if somebody pulled master at just the right moment, d2l-fetch-simple-cache.html would be pointing to a non-existant file, but oh well. (If the consumer is using a proper bower version, this situation can't occur since the new version is only generated after the new file is CDN-ified.)
    
    This also removes frau-ci, as it has some issues with this approach; instead, we more or less accomplish the same steps with the shell script.

---
+++ update.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+if ! [ "$TRAVIS_BRANCH" == "master" ] || ! [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	echo "Version is only bumped on master"
+	exit 0
+fi
+
+lastVersion=$(git describe --abbrev=0)
+versionRegex='^([0-9]+)\.([0-9]+)\.([0-9]+)$'
+if ! [[ $lastVersion =~ $versionRegex ]]; then
+	echo $lastVersion "is not a valid semver string"
+	exit 1
+fi
+
+majorVersion=${BASH_REMATCH[1]}
+minorVersion=${BASH_REMATCH[2]}
+patchVersion=${BASH_REMATCH[3]}
+
+lastLogMessage=$(git log -1 --pretty=format:%s%b)
+lastLogMessageShort=$(git log -1 --pretty=format:%s)
+
+majorRegex='\[increment major\]'
+patchRegex='\[increment patch\]'
+if [[ $lastLogMessage =~ $majorRegex ]]; then
+	majorVersion=$((majorVersion + 1))
+elif [[ $lastLogMessage =~ $patchRegex ]]; then
+	patchVersion=$((patchVersion + 1))
+else
+	minorVersion=$((minorVersion + 1))
+fi
+
+newVersion="${majorVersion}.${minorVersion}.${patchVersion}"
+
+# Add the upstream using GITHUB_RELEASE_TOKEN
+git remote add upstream "https://${GITHUB_RELEASE_TOKEN}@github.com/Brightspace/d2l-fetch-simple-cache.git"
+
+# Pull the merge commit
+git pull upstream master
+git checkout upstream/master
+
+# Set config so this commit shows up as coming from Travis
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+
+echo "Updating from ${lastVersion} to ${newVersion}"
+echo "<!-- CHANGES TO THIS FILE WILL BE LOST - IT IS AUTOMATICALLY GENERATED WHEN d2l-fetch-simple-cache IS RELEASED -->" > d2l-fetch-simple-cache.html
+echo "<script src=\"https://s.brightspace.com/lib/d2lfetch-simple-cache/"$newVersion"/d2lfetch-simple-cache.js\"></script>" >> d2l-fetch-simple-cache.html
+
+# Add the updated d2l-fetch-simple-cache.html, and add a new tag to create the release
+git add .
+git commit -m "[skip ci] Update to ${newVersion}"
+git tag -a ${newVersion} -m "${newVersion} - ${lastLogMessageShort}"
+
+git status
+
+git push upstream HEAD:master --tags
+
+# Publish the release via frau-publisher
+export TRAVIS_TAG=$newVersion
+npm run publish-release
