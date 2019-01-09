33ecfe6217701c69d3e48d99b3bfd5affe470ef2
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2019-01-09 11:00:04 -0500

    Polymer 3 prep - Use polymer-cli and rollup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { D2LFetchSimpleCache } from './d2lfetch-simple-cache.js';
 
 const fetchSimpleCache = new D2LFetchSimpleCache();
 
-module.exports = function cache(request, next, options) {
+export default function cache(request, next, options) {
 	return fetchSimpleCache.cache(request, next, options);
-};
+}
