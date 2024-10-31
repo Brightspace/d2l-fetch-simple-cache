de79ed6f37e9368ef46f2f276df1122c6e864023
GZolla <43836485+GZolla@users.noreply.github.com>
2024-10-31 14:11:57 -0700

    Update to eslint9

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { includeIgnoreFile } from "@eslint/compat";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default [
+    includeIgnoreFile(gitignorePath),
+    ...compat.extends("brightspace/browser-config").map(c => ({
+		...c,
+		files:['**/*.js', '**/*.html']
+	})),
+    ...compat.extends("brightspace/testing-config").map(c => ({
+		...c,
+		files:['test/**/*']
+	})),
+];
