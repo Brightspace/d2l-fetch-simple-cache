7cfc7c3163b14ca9d9bf87ba36c648fc861059ed
GZolla <43836485+GZolla@users.noreply.github.com>
2024-10-31 14:17:11 -0700

    Appease Linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import path from 'node:path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
 });
-const gitignorePath = path.resolve(__dirname, ".gitignore");
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
-    includeIgnoreFile(gitignorePath),
-    ...compat.extends("brightspace/browser-config").map(c => ({
+	includeIgnoreFile(gitignorePath),
+	...compat.extends('brightspace/browser-config').map(c => ({
 		...c,
 		files:['**/*.js', '**/*.html']
 	})),
-    ...compat.extends("brightspace/testing-config").map(c => ({
+	...compat.extends('brightspace/testing-config').map(c => ({
 		...c,
 		files:['test/**/*']
 	})),
